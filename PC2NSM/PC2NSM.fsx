7f93cc30a8590395d7ead6362396036d34345b87
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-01-31 01:10:35 +0100

    Fixed recursion operatator and list bug

--- PC2NSM/PC2NSM.fsx
+++ PC2NSM/PC2NSM.fsx
@@ -9,30 +9,46 @@
 //11	16	mammals
 //12	13	tiger
 //14	15	horse
+//    let root = {label="animals"; nsm={lft=1;rgt=18};pc={id=0;parentId=0}}
+//    let vertebrates = {label="vertebrates";nsm={lft=2;rgt=9};pc={id=1;parentId=0}}
+//    let invertebrates = {label="invertebrates";nsm={lft=10;rgt=17};pc={id=2;parentId=0}}
+//    let mollusks = {label="mollusks";nsm={lft=3;rgt=4};pc={id=3;parentId=1}}          
+//    let insects = {label="insects";nsm={lft=5;rgt=8};pc={id=4;parentId=1}} 
+//    let mantis = {label="mantis";nsm={lft=6;rgt=7};pc={id=5;parentId=4}} 
+//    let mammals = {label="mammals";nsm={lft=11;rgt=16};pc={id=6;parentId=2}} 
+//    let tiger = {label="tiger";nsm={lft=12;rgt=13};pc={id=7;parentId=6}}
+//    let horse = {label="horse";nsm={lft=14;rgt=15};pc={id=8;parentId=6}}
+//    let NSMtree = [root,vertebrates,invertebrates,mollusks,insects,mantis,mammals,tiger,horse] 
 
 //module RecordTypes = 
-
-    // define a record type
-    type NSMNode = 
-        { label     : string;
-          lft    : int;
+    type NSM = 
+        { lft    : int;
           rgt : int }
-    let root = {label="animals";lft=1;rgt=18}
-    let vertebrates = {label="vertebrates";lft=2;rgt=9}
-    let invertebrates = {label="invertebrates";lft=10;rgt=17}
-    let mollusks = {label="mollusks";lft=3;rgt=4}          
-    let insects = {label="insects";lft=5;rgt=8} 
-    let mantis = {label="mantis";lft=6;rgt=7} 
-    let mammals = {label="mammals";lft=11;rgt=16} 
-    let tiger = {label="tiger";lft=12;rgt=13}
-    let horse = {label="horse";lft=14;rgt=15}
-    let NSMtree = [root,vertebrates,invertebrates,mollusks,insects,mantis,mammals,tiger,horse] 
+    type PC = 
+        {   id:int;
+            parentId:int}
+    type Node = 
+        {   label:string;
+            nsm:NSM;
+            pc:PC}
+//Fill with fake data
+    let root:Node = {label="animals"; nsm={lft=1;rgt=18};pc={id=0;parentId=0}}
+    let vertebrates:Node = {label="vertebrates";nsm={lft=2;rgt=9};pc={id=1;parentId=0}}
+    let invertebrates:Node = {label="invertebrates";nsm={lft=10;rgt=17};pc={id=2;parentId=0}}
+    let mollusks:Node = {label="mollusks";nsm={lft=3;rgt=4};pc={id=3;parentId=1}}          
+    let insects:Node = {label="insects";nsm={lft=5;rgt=8};pc={id=4;parentId=1}} 
+    let mantis:Node = {label="mantis";nsm={lft=6;rgt=7};pc={id=5;parentId=4}} 
+    let mammals:Node = {label="mammals";nsm={lft=11;rgt=16};pc={id=6;parentId=2}} 
+    let tiger:Node = {label="tiger";nsm={lft=12;rgt=13};pc={id=7;parentId=6}}
+    let horse:Node = {label="horse";nsm={lft=14;rgt=15};pc={id=8;parentId=6}}
+    let NSMtree:Node list  = [root;vertebrates;invertebrates;mollusks;insects;mantis;mammals;tiger;horse] 
     let showNode n =
-        n.label 
-        
-        
-//    let printAllNSMNodes() = 
-//        for node in NSMtree do 
-//                  showNode vertebrates
+        printfn "Name of Node is:%s parent is:%d" n.label n.pc.parentId
 
-    showNode insects  ;;
+    let foo = (1,2,3,4)       
+    let rec processItems proc = function
+      | []       -> ()
+      | hd :: tl ->
+          proc hd;
+          processItems proc tl // recursively enumerate list
+    processItems  showNode NSMtree ;;
